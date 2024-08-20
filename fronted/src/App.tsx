f26a358deea7e9f71f0363925193a6d015706a6d
jasernan <julian-309@outlook.com>
2024-08-20 18:32:21 -0500

    Lista de productos

--- fronted/src/App.tsx
+++ fronted/src/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
 import './App.css'
+import { sampleProducts } from './data'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,7 +9,21 @@ function App() {
   return (
     <div>
       <header>TS amazona</header>
-      <main></main>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img
+                src={product.image}
+                alt={product.name}
+                className="product-image"
+              />
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All right reserved</footer>
     </div>
   )
